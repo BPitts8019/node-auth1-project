5e1ed996ac603bfc33dc7ccd6ef0440695cb974a
BPitts8019 <bradleyj.pitts@gmail.com>
2020-01-17 18:54:04 -0700

    Configured session cookies... I think :bug:
    I've added express-session in to the project and created an instance with some
    options. When I test with login endpoint, I don't see a cookie being created.
    I've obviously overlooked something; will pick it up again tomorrow.

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const server = express();
 
 //middleware
 const helmet = require("helmet");
-const bcrypt = require("bcryptjs");
 const credentialsInput = require("./auth/credentialsInput");
 const restricted = require("./auth/restricted");
+const session = require("express-session");
 
 
 //data models
@@ -14,6 +14,16 @@ const users_db = require("./users/users-model");
 //apply middleware
 server.use(helmet());
 server.use(express.json());
+server.use(session({
+   resave: false,
+   saveUninitialized: false,
+   secret: process.env.SECRET_PHRASE,
+   cookie: {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 30,//expires after 30min
+      secure: false
+   },
+}));
 
 //Welcome
 server.get("/", (req, res, next) => {
